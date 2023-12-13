fcbc5a46cd460391e7d97a7e2758024da0fb18af
aicha652 <aichaazlf652@gmail.com>
2023-12-13 05:08:17 -0800

    fix error

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -7,9 +7,14 @@ User class
 class User():
     """ Documentation """
 
-    def __init__(self, email=None):
+    def __init__(self, email):
         """ Documentation """
         self.__email = email
+
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
 
     @email.setter
     def email(self, value):
@@ -18,14 +23,8 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-
 
 if __name__ == "__main__":
 
-    u = User()
-    u.email = "john@snow.com"
+    u = User("john@snow.com")
     print(u.email)
